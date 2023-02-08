e1d4df26db64de1e558d6dbdfc6b19e576fcb6be
SuzukiYudai <kuroko0725cc@gmail.com>
2023-02-08 18:32:20 +0900

    add function todo-add done

---
+++ app.js
@@ -0,0 +1,8 @@
+$("#addTodo").click(function () {
+  const inputTodo = $("input").val();
+  $("#todoList").append(
+    "<li><input type = 'checkbox' />" + inputTodo + "</li>"
+  );
+
+  $("input").val("");
+});
